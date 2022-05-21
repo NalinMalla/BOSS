a9aee80f2473657fdfac9dc8e0bbca270a46e0cb
NalinMalla <nilanallam@gmail.com>
2022-05-21 18:09:02 +0545

    Setup index.js for images and colors

---
+++ src/res/colors/index.js
@@ -0,0 +1,9 @@
+const Colors = {
+  primary: '#361709',
+  secondary: '#595959',
+  black: '#000000',
+  biraOlive: '#666734',
+  biraRed: '#A72B0B',
+}
+
+export default Colors;
---
+++ src/res/images/index.js
@@ -0,0 +1,11 @@
+import bb from './bb.jpg';
+import bbb from './bbb.jpg';
+import bbbb from './bbbb.jpg';
+
+const Images= {
+  backgroundImage1: bb,
+  backgroundImage2: bbb,
+  backgroundImage3: bbbb,
+}
+
+export default Images;

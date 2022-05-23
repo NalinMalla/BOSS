cc734d3cbc4215d449545f1f7383a341e432123c
NalinMalla <nilanallam@gmail.com>
2022-05-23 23:13:37 +0545

    Add images.

--- src/res/images/index.js
+++ src/res/images/index.js
@@ -1,13 +1,35 @@
+import Bed from './bed.jpg';
+import Chair from './chair.jpg';
+import Dresser from './dresserBrown.jpg';
+import Logo from './logo.png';
 import MirrorGreen from './mirrorGreen.jpg';
 import MirrorWhite from './mirrorWhite.jpg';
+import Prefab from './prefab.jpeg';
+import RoomARWhite from './roomARWhite.jpg';
+import RoomBrown from './roomBrown.jpg';
 import RoomGreen from './roomGreen.jpg';
+import RoomLightBrown from './roomLightBrown.jpg';
+import Shelf from './shelf.jpg';
+import SideTable from './sideTableBrown.jpg';
 import SofaBrown from './sofaBrown.jpg';
+import Window from './window.jpg';
 
 const Images= {
+  Bed: Bed,
+  Chair: Chair,
+  Dresser: Dresser,
+  Logo: Logo,
   MirrorGreen: MirrorGreen,
   MirrorWhite: MirrorWhite,
+  Prefab: Prefab,
+  RoomARWhite: RoomARWhite,
+  RoomBrown: RoomBrown,
   RoomGreen: RoomGreen,
+  RoomLightBrown: RoomLightBrown,
+  Shelf: Shelf,
+  SideTable: SideTable,
   SofaBrown: SofaBrown,
+  Window: Window,
 }
 
 export default Images;

40d570c6d28bd4581370f9797cda50e925212b6b
NalinMalla <nilanallam@gmail.com>
2022-05-25 09:53:39 +0545

    Add Antic Slab Font

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { ThemeProvider } from "@mui/material/styles";
+import './App.css';
 
 import Theme from "./theme";
 import Routes from './routes';
 
 export default function App() {
-  return (
+  return ( 
     <ThemeProvider theme={Theme}>
       <Routes/>
     </ThemeProvider>

15de4935748d6bd9ef97179056400e9b4c89e003
NalinMalla <nilanallam@gmail.com>
2022-05-26 09:56:49 +0545

    Add Images.

--- src/res/images/index.js
+++ src/res/images/index.js
@@ -1,9 +1,16 @@
-import Bed from './bed.jpg';
-import Chair from './chair.jpg';
+import Bed from './bedM.jpg';
+import Chair from './chairM.jpg';
+import Door from './doorM.jpg';
 import Dresser from './dresserBrown.jpg';
-import Logo from './logo.png';
+import ExchangeOffer1 from './offers/exchangeOffer1.jpg';
+import ExchangeOffer2 from './offers/exchangeOffer2.jpg';
+import Logo from './logo/logo.svg';
+import LogoStamp from './logo/logoStamp.svg';
 import MirrorGreen from './mirrorGreen.jpg';
 import MirrorWhite from './mirrorWhite.jpg';
+import MirrorBage from './mirrorBage.jpg';
+import Decoration from './decorationM.jpg';
+import OnlineShopping from './offers/onlineShopping.jpeg';
 import Prefab from './prefab.jpeg';
 import RoomARWhite from './roomARWhite.jpg';
 import RoomBrown from './roomBrown.jpg';
@@ -11,16 +18,24 @@ import RoomGreen from './roomGreen.jpg';
 import RoomLightBrown from './roomLightBrown.jpg';
 import Shelf from './shelf.jpg';
 import SideTable from './sideTableBrown.jpg';
-import SofaBrown from './sofaBrown.jpg';
-import Window from './window.jpg';
+import SofaBrown from './sofaM.jpg';
+import Table from './tableM.jpg';
+import Wardrobe from './wardrobeM.jpg';
+import Window from './windowM.jpg';
+import HomeDesign from './designs/homeDesign1.svg';
 
 const Images= {
   Bed: Bed,
   Chair: Chair,
+  Door: Door,
   Dresser: Dresser,
+  ExchangeOffer1: ExchangeOffer1,
+  ExchangeOffer2: ExchangeOffer2,
   Logo: Logo,
+  LogoStamp: LogoStamp,
   MirrorGreen: MirrorGreen,
   MirrorWhite: MirrorWhite,
+  OnlineShopping: OnlineShopping,
   Prefab: Prefab,
   RoomARWhite: RoomARWhite,
   RoomBrown: RoomBrown,
@@ -29,7 +44,12 @@ const Images= {
   Shelf: Shelf,
   SideTable: SideTable,
   SofaBrown: SofaBrown,
+  Table: Table,
+  Wardrobe: Wardrobe,
   Window: Window,
+  HomeDesign: HomeDesign,
+  Decoration: Decoration,
+  MirrorBage: MirrorBage,
 }
 
 export default Images;

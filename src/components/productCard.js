0079867b6be778f7ca987ab310651355cc72b0d8
NalinMalla <nilanallam@gmail.com>
2022-05-27 19:02:13 +0545

    Update priceCut.

--- src/components/productCard.js
+++ src/components/productCard.js
@@ -3,41 +3,41 @@
 import Colors from "../res/colors";
 
 export default function ProductCard(props) {
-  let priceCut="";
-  if(props.priceDiscount!== "")
-  {
-    priceCut = {
-      textDecoration: 'line-through',
-      marginRight: 5,
-    };
-  }
-  else
-  {
-    priceCut="";
-  }
   return (
-    <div style={{...Styles.root, ...props.style,}}>
+    <div style={{ ...Styles.root, ...props.style }}>
       <div
         style={{
           width: 196,
           height: 196,
           background: `url(${props.image})`,
           backgroundSize: "cover",
-          borderRadius: '8px 8px 0px 0px',
+          borderRadius: "8px 8px 0px 0px",
           border: `1px solid ${Colors.primary}`,
         }}
-      >
-      </div>
+      ></div>
       <div style={Styles.container}>
         <span style={Styles.text}>{props.title}</span>
         <span style={Styles.text}>
-          <span style={{...priceCut}}>{props.price}</span>
-          <span>{props.priceDiscount}</span>
-        </span>  
-      </div> 
+          <span
+            style={{
+              textDecoration: props.discountedPrice === 0 ? "none" : "line-through",
+              marginRight: props.discountedPrice === 0 ? 0 : 5,
+            }}
+          >
+            {props.price}
+          </span>
+          <span>{props.discountedPrice}</span>
+        </span>
+      </div>
     </div>
   );
 }
+
+ProductCard.defaultProps = {
+  price: 0,
+  discountedPrice: 0,
+  style: {},
+};
 
 const Styles = {
   root: {
@@ -47,7 +47,7 @@ const Styles = {
     alignItems: "stretch",
     width: 196,
     height: 254,
-    borderRadius: '8px',
+    borderRadius: "8px",
     boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
   },
   container: {
@@ -59,12 +59,12 @@ const Styles = {
     alignItems: "center",
     color: "#FFFFFFFF",
     height: 58,
-    borderRadius: '0px 0px 8px 8px',
+    borderRadius: "0px 0px 8px 8px",
     border: `1px solid ${Colors.primary}`,
   },
   text: {
-    width: '90%',
-    height: '50%',
+    width: "90%",
+    height: "50%",
     marginTop: 2,
     marginBottom: 2,
   },

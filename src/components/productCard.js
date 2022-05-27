fa4ef59a6cccdede5a395456afc5b182738b7250
NalinMalla <nilanallam@gmail.com>
2022-05-27 12:00:57 +0545

    Add productGrid and priceCut in productCard.

--- src/components/productCard.js
+++ src/components/productCard.js
@@ -1,17 +1,40 @@
-import { Link } from "@mui/material";
+// import { Link } from "@mui/material";
+
+import Colors from "../res/colors";
 
 export default function ProductCard(props) {
+  let priceCut="";
+  if(props.priceDiscount!== "")
+  {
+    priceCut = {
+      textDecoration: 'line-through',
+      marginRight: 5,
+    };
+  }
+  else
+  {
+    priceCut="";
+  }
   return (
-    <div style={Styles.root}>
+    <div style={{...Styles.root, ...props.style,}}>
       <div
         style={{
-          ...Styles.container,
+          width: 196,
+          height: 196,
           background: `url(${props.image})`,
           backgroundSize: "cover",
+          borderRadius: '8px 8px 0px 0px',
+          border: `1px solid ${Colors.primary}`,
         }}
-      ></div>
-      <span style={Styles.title}>{props.title}</span>
-      <span style={Styles.title}>{props.price}</span>
+      >
+      </div>
+      <div style={Styles.container}>
+        <span style={Styles.text}>{props.title}</span>
+        <span style={Styles.text}>
+          <span style={{...priceCut}}>{props.price}</span>
+          <span>{props.priceDiscount}</span>
+        </span>  
+      </div> 
     </div>
   );
 }
@@ -20,26 +43,29 @@ const Styles = {
   root: {
     display: "flex",
     flexDirection: "column",
-    justifyContent: "flex-start",
-    alignItems: "center",
-    width: 210,
-    height: 266,
-    borderRadius: 8,
-    border: "1px solid #361709",
+    justifyContent: "space-between",
+    alignItems: "stretch",
+    width: 196,
+    height: 254,
+    borderRadius: '8px',
     boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
   },
   container: {
-    
-  },
-  title: {
-    backgroundColor: "rgba(0, 0, 0, 0.4)",
+    backgroundColor: Colors.primary,
     width: "100%",
     display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
+    flexDirection: "column",
+    justifyContent: "stretch",
     alignItems: "center",
     color: "#FFFFFFFF",
-    height: 40,
-    borderRadius: "8px 8px 0px 0px",
+    height: 58,
+    borderRadius: '0px 0px 8px 8px',
+    border: `1px solid ${Colors.primary}`,
+  },
+  text: {
+    width: '90%',
+    height: '50%',
+    marginTop: 2,
+    marginBottom: 2,
   },
 };
---
+++ src/components/productGrid.js
@@ -0,0 +1,72 @@
+import * as React from "react";
+
+import ProductCard from "./productCard";
+
+import Images from "../res/images";
+
+const ProductCardData = [
+    {
+      title: "Grey Sofa",
+      image: Images.SofaBrown,
+      price: 'Rs.30,000',
+      url: "product/sofa",
+      
+    },
+    {
+      title: "Ergonomic Chair",
+      image: Images.Chair,
+      price: 'Rs.99,99,999',
+      priceDiscount: 'Rs.10,00,000',
+      url: "product/chair",
+    },
+    {
+      title: "BED",
+      image: Images.Bed,
+      price: 'Rs.30,000',
+      url: "product/bed",
+    },
+    {
+      title: "Ply Wood Table",
+      image: Images.Table,
+      price: 'Rs. 20,000',
+      url: "product/table",
+    },
+    {
+      title: "SHELF",
+      image: Images.Shelf,
+      price: 'Rs.30,000',
+      url: "category/shelf",
+    },
+    {
+      title: "DOOR",
+      image: Images.Door,
+      price: 'Rs.20,000',
+      url: "category/door",
+    }
+];
+const ProductGrid = () => {
+  return (
+      <div style={Styles.container}>
+        {ProductCardData.map((element) => (
+          <ProductCard {...element} style= {Styles.card}/>
+        ))}
+      </div>
+  );
+};
+
+const Styles = {
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    marginTop: 32,
+    width: '90%',
+  },
+  card: {
+    marginLeft: 10,
+    marginRight: 10,
+  }
+};
+
+export default ProductGrid;

434e4d0113394f00488587b83b11fa503dc1b8a0
NalinMalla <nilanallam@gmail.com>
2022-08-20 20:53:05 +0545

    Include redux admin logout in logout.js

--- src/components/logout.js
+++ src/components/logout.js
@@ -1,5 +1,10 @@
+import { useDispatch } from "react-redux";
+
+import { updateIsAdmin } from "../redux/reducer/isAdmin";
 
 export default function Logout(){
+  const dispatch = useDispatch();
+  dispatch(updateIsAdmin(false));
   localStorage.clear();
   window.location.href = "/";
 }
